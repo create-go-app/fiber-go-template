f66ab36d3d10a5d6450455129ba4143341bc7ba0
Vic Shóstak <vikkyshostak@gmail.com>
2021-03-04 22:30:29 +0300

    Update JWT midware

--- pkg/middleware/jwt_middleware.go
+++ pkg/middleware/jwt_middleware.go
@@ -16,8 +16,8 @@ func JWTProtected() fiber.Handler {
 		SigningKey: []byte(os.Getenv("JWT_SECRET_TOKEN")),
 		ContextKey: "jwt", // used in private routes
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			// Return status 403 and failed authentication error.
-			return c.Status(403).JSON(fiber.Map{
+			// Return status 401 and failed authentication error.
+			return c.Status(401).JSON(fiber.Map{
 				"error": true,
 				"msg":   err.Error(),
 			})
